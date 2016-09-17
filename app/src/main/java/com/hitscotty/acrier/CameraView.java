478c64a67879c41578cbba1a2b9b1c602c2184e0
Jonathan <Jportorreal77@gmail.com>
2016-09-17 16:44:06 -0400

    Adding Android Acrier

---
+++ app/src/main/java/com/hitscotty/acrier/CameraView.java
@@ -0,0 +1,18 @@
+package com.hitscotty.acrier;
+
+import android.content.Context;
+import android.graphics.Camera;
+import android.opengl.GLSurfaceView;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Created by Scotty on 9/17/16.
+ */
+public class CameraView {
+
+    private SurfaceHolder mHolder;
+    private Camera mCamera;
+
+
+}
