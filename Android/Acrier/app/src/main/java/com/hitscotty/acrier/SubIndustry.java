de2b7e1ed03a8dbb8cdb08c1b206edf07de8cbad
Jonathan <Jportorreal77@gmail.com>
2016-09-17 18:00:34 -0400

    finished android landing pages

--- Android/Acrier/app/src/main/java/com/hitscotty/acrier/SubIndustry.java
+++ Android/Acrier/app/src/main/java/com/hitscotty/acrier/SubIndustry.java
@@ -3,11 +3,11 @@ package com.hitscotty.acrier;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Industry extends AppCompatActivity {
+public class SubIndustry extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sub_industry);
     }
 }
